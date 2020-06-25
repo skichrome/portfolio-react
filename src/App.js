bfc9882063150b3678cd305fb4b6f9bcca4a69a2
skichrome <toni.campeol@outlook.fr>
2020-06-25 11:54:07 +0200

    Ajout Firebase (cloud firestore)
    Remplacement du json statique de données pour les catégories de projet par la BDD Cloud Firestore
    + Fix d'un bug de routes dans App.js
    + Ajout du fichier des credentials Firebase dans .gitignore (pour l'instant)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
 import Home from './containers/Home';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Contact from './containers/Contact';
 import Project from './containers/Project';
 import ProjectList from './containers/ProjectList';
-import 'materialize-css/dist/css/materialize.min.css';
-import 'materialize-css/dist/js/materialize.min.js';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
@@ -18,7 +18,7 @@ function App() {
         <Route path="/" exact component={Home}/>
         <Route path="/projects" component={ProjectList}/>
         <Route path="/project/:projectId" component={Project}/>
-        <Route path="/contact" component={Contact}/>
+        <Route path="/about-me" component={Contact}/>
         <Footer/>
       </Router>
     </div>
--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -1,36 +1,49 @@
 import React, { useState, useEffect } from 'react';
 import 'materialize-css';
 import { Icon, Card, CardTitle, Row, Col } from 'react-materialize';
-import ProjectsData from '../../data/projects-data.json'
 import { NavLink } from 'react-router-dom';
+import { database } from '../../firebase';
 
 /**
 * @author
 * @function Category
 **/
 
-const Category = (props) => {
-    const [projectDataList, setProjectDataList] = useState([]);
+const Category = () => {
+    const [categories, setCategories] = useState([]);
 
     useEffect(() => {
-        const projectDataList = ProjectsData;
-        setProjectDataList(projectDataList);
-    },[projectDataList]);
+        console.log('effect');
+        const categoriesFromFirestore = database.collection('portfolio')
+            .doc('V1.0')
+            .collection('categories')
+            .onSnapshot(snapshot => {
+                const allCategories = snapshot.docs.map(category => ({
+                    id: category.id,
+                    ...category.data()
+                }));
+                setCategories(allCategories);
+            });
+        return () => {
+            console.log('cleanup');
+            categoriesFromFirestore();
+        }
+    },[]);
 
   return(
-    projectDataList.map(projectData => {
-        const imgSrc = projectData.picture ? require('../../assets/' + projectData.picture) : null;
+    categories.map(category => {
+        const imgSrc = category.picture ? require('../../assets/' + category.picture) : null;
         return(
-            <Row key={projectData.id}>
+            <Row key={category.id}>
                 <Col l={10} offset={'l1'} s={12}>
-                    <Card actions={[<NavLink key={projectData.id} to={`/category/${projectData.id}`}><Icon right>arrow_forward</Icon> En savoir plus</NavLink>]}
-                        title={projectData.name}
+                    <Card actions={[<NavLink key={category.id} to={`/category/${category.id}`}><Icon right>arrow_forward</Icon> En savoir plus</NavLink>]}
+                        title={category.name}
                         textClassName="white-text"
                         className="light-blue darken-2"
                         header={ imgSrc ? <CardTitle image={imgSrc} alt='rees' className="valign-wrapper"></CardTitle> : null }
                         horizontal
                     >
-                        <p>{projectData.desc}</p>
+                        <p>{category.desc}</p>
                     </Card>
                 </Col>
             </Row>
